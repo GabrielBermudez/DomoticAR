df300d8047aecb150d5a5a3ef03117c330534e03
Gabriel M. Bermudez <gabrielbermudez0@gmail.com>
2022-03-16 22:39:14 -0300

    Agrego primer desafio

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Mi primer prueba de App!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'red',
+    fontSize: 30,
+    textDecorationLine:'underline',
+  }
+});
